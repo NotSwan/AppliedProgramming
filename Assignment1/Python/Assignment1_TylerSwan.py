c0beb2e26b4ff531a88a17a210fd13a5ae7ee881
NotSwan <31709744+NotSwan@users.noreply.github.com>
2022-05-24 14:21:57 -0400

    Added menu functionality  (.py)

--- Assignment1/Python/Assignment1_TylerSwan.py
+++ Assignment1/Python/Assignment1_TylerSwan.py
@@ -1,7 +1,7 @@
 ###############
 #Citations
 #https://www.w3schools.com/python/python_inheritance.asp
-#
+#https://stackoverflow.com/questions/23326099/how-can-i-limit-the-user-input-to-only-integers-in-python
 #
 ###############
 class User:
@@ -25,16 +25,22 @@ class User:
         self.id = id
     
     def get_user(self):
-        print (self.lastName + ", " + self.firstName + "\nWIT IT: W00" + str(self.id))
+        print (self.lastName + ", " + self.firstName + "\nWIT ID: W00" + str(self.id))
 
 class Student(User):
     def __init__(self, firstName = "studentFirst", lastName = "studentLast", id = 1235):
         User.__init__(self, firstName, lastName, id)
 
+    def debug(self):
+        self.get_user()
+        self.search_classes()
+        self.add_drop_class()
+        self.print_schedule()
+
     def search_classes(self):
         print ("This will search classes")
 
-    def add_drop_class(self, course):
+    def add_drop_class(self):
         print ("This will add/drop sepcified class")
 
     def print_schedule(self):
@@ -43,6 +49,12 @@ class Student(User):
 class Instructor(User):
     def __init__(self, firstName = "instructorFirst", lastName = "instructorLast", id = 1236):
         User.__init__(self, firstName, lastName, id)
+
+    def debug(self):
+        self.get_user()
+        self.print_course_list()
+        self.print_schedule()
+        self.search_classes()
 
     def print_schedule(self):
         print ("This will print the schedule")
@@ -57,11 +69,18 @@ class Admin(User):
     def __init__(self, firstName = "adminFirst", lastName = "adminLast", id = 1237):
         User.__init__(self, firstName, lastName, id)
 
+    def debug(self):
+        self.get_user()
+        self.add_course()
+        self.remove_course()
+        self.add_remove_student()
+        self.search_classes()
+
     def add_course(self, course):
-        print ("This will add/drop sepcified class")
+        print ("This will add specified class")
 
     def remove_course(self, course):
-        print ("This will add/drop sepcified class")
+        print ("This will remove specified class")
     
     def add_remove_student(self, student):
         print ("This will add/remove students")
@@ -70,19 +89,80 @@ class Admin(User):
         print ("This will search courses and print their roster")
 
 
-def __main__():
-    user = User("Fortnite", "Battlepass", 42069)
-    user.get_user()
-    print()
-    student = Student(id=124)
-    student.get_user()
-    student.print_schedule()
-    student.search_classes()
-    print()
-    instructor = Instructor("Me", "Kill", 666)
-    instructor.get_user()
-    print()
-    admin = Admin()
-    admin.get_user()
 
-__main__()
+def prompt_for_usertype():
+    print("To begin what type of user would you like to create?")
+    print("1. User")
+    print("2. Student")
+    print("3. Instructor")
+    print("4. Admin")
+    print("0. Exit")
+
+    while True:
+        try:
+            userType = int(input("Input num: "))
+            if(userType >= 0 and userType < 5):
+                return userType
+            else:
+                raise Exception("IntegerOutOfRange")
+        except:
+            print("Invalid input!")
+
+def create_user(userType, users):
+    if(userType != 0):
+        print(f"\nYou selected: {userTypeDict.get(userType)}")
+        print("Enter Credentials seperated by commas(,) as first,last,id")
+        
+        while True:
+            try:
+                a,b,c = input("Input:").split(",")
+                int(c)
+                break
+            except:
+                print("Invalid input")
+        if(userType == 1):
+            users.append(User(a,b,c))
+        if(userType == 2):
+            users.append(Student(a,b,c))
+        if(userType == 3):
+            users.append(Instructor(a,b,c))
+        if(userType == 4):
+            users.append(Admin(a,b,c))
+
+def menu():
+    print("Main Menu")
+    print("5. Debug(calls all other commands)")
+    print("0. Exit")
+
+    while True:
+        try:
+            userType = int(input("Input num: "))
+            if(userType >= 0 and userType < 6):
+                break
+            else:
+                raise Exception("IntegerOutOfRange")
+        except:
+            print("Invalid input!")
+
+    users[0].debug()
+    return userType
+
+
+if __name__ == '__main__':
+    global userTypeDict 
+    userTypeDict= {
+        1: "User",
+        2: "Student",
+        3: "Instructor",
+        4: "Admin"
+    }
+
+    print("\t---Welcome to the User Creation Tool---")
+    userInput = prompt_for_usertype()
+    users = list()
+    create_user(userInput, users)
+    
+    while userInput != 0:
+        userInput = menu()
+        
+    print("Exiting...")
